57287b61216056be8aa316ff25c8d25ed50952ad
rafaelabarbieric <barbieridacruzrafaela@gmail.com>
2023-12-05 16:55:46 -0300

    commit add teclado

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 let conta = document.querySelector("#display")
 let operador;
+let btnTotal = document.querySelector("#btnTotal");
 
 function add_numero(n) {
     conta.value = conta.value + n;
+    btnTotal.focus();
 }
 
 function add_operador(op) {
     conta.value = conta.value + op;
     operador = op;
+    btnTotal.focus();
 }
 
-function add_numero(n) {
-    conta.value = conta.value + n;
-}
+document.addEventListener('keydown', function(event) {
+    if (isNaN(event.key) == false || event.key == "/" || event.key == "*" || event.key == "+" || event.key == "-") {
+        conta.value = conta.value + event.key;
+    }
+    if (event.key == 'Enter') {
+        total();
+    }
+})
 
 function total(){
     let contaAnterior = document.querySelector("#history");
     contaAnterior.innerHTML = conta.value;
 
     numero = conta.value.split(operador);
-    console.log(numero);
 
     switch (operador) {
         case "+":
@@ -43,3 +50,4 @@ function total(){
 function limpar(){
     conta.value = "";
 }
+
